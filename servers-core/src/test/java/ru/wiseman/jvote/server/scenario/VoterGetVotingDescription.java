cdcfd534ddc84015f464045eea2d7b690957f0fd
Mikhail Vaysman <9996113+github@gmail.com>
2009-10-16 02:43:12 +0400

    changed scenario's file extension. accordingly updated scenario's java class

--- servers-core/src/test/java/ru/wiseman/jvote/server/scenario/VoterGetVotingDescription.java
+++ servers-core/src/test/java/ru/wiseman/jvote/server/scenario/VoterGetVotingDescription.java
@@ -1,6 +1,12 @@
 package ru.wiseman.jvote.server.scenario;
 
+import org.jbehave.scenario.MostUsefulConfiguration;
+import org.jbehave.scenario.PropertyBasedConfiguration;
 import org.jbehave.scenario.Scenario;
+import org.jbehave.scenario.parser.ClasspathScenarioDefiner;
+import org.jbehave.scenario.parser.PatternScenarioParser;
+import org.jbehave.scenario.parser.ScenarioDefiner;
+import org.jbehave.scenario.parser.UnderscoredCamelCaseResolver;
 import ru.wiseman.jvote.server.scenario.steps.GetingVotingDescriptionSteps;
 
 public class VoterGetVotingDescription extends Scenario
@@ -11,6 +17,11 @@ public class VoterGetVotingDescription extends Scenario
     }
     
     public VoterGetVotingDescription(final ClassLoader classLoader) {
-        super(new GetingVotingDescriptionSteps());
+        super(new MostUsefulConfiguration() {
+            public ScenarioDefiner forDefiningScenarios() {
+                return new ClasspathScenarioDefiner(new UnderscoredCamelCaseResolver(".scenario"),
+                               new PatternScenarioParser(new PropertyBasedConfiguration()), classLoader);
+            }
+        }, new GetingVotingDescriptionSteps());
     }
 }
