11081028031352e30a481adb22c18b1df690e9c0
Mikhail Vaysman <9996113+github@gmail.com>
2009-10-16 06:05:03 +0400

    removed useless typecast

--- servers-core/src/test/java/ru/wiseman/jvote/server/scenario/VoterGetVotingDescription.java
+++ servers-core/src/test/java/ru/wiseman/jvote/server/scenario/VoterGetVotingDescription.java
@@ -1,11 +1,10 @@
 package ru.wiseman.jvote.server.scenario;
 
-import org.jbehave.scenario.steps.CandidateSteps;
 import ru.wiseman.jvote.server.scenario.steps.GetingVotingDescriptionSteps;
 import ru.wiseman.utils.test.jbehave.MyScenario;
 
 public class VoterGetVotingDescription extends MyScenario {
     public VoterGetVotingDescription(final ClassLoader classLoader) {
-        super(classLoader, (CandidateSteps) new GetingVotingDescriptionSteps());
+        super(classLoader, new GetingVotingDescriptionSteps());
     }
 }
