b598f80682a90a88f9376f29a2bff1b677a35c40
Mikhail Vaysman <9996113+github@gmail.com>
2009-10-29 18:20:32 +0300

    added VoterId class (stub)

---
+++ model/src/main/java/ru/wiseman/jvote/model/VoterId.java
@@ -0,0 +1,11 @@
+package ru.wiseman.jvote.model;
+
+import java.io.Serializable;
+
+/**
+ * User: vaysman
+ * Date: 29.10.2009
+ * Time: 18:18:02
+ */
+public class VoterId implements Serializable {
+}
