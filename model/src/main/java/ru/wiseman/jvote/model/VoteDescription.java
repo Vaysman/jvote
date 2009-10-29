dce64879246d2e70c1059ef403fffb740c98379c
Mikhail Vaysman <9996113+github@gmail.com>
2009-10-29 16:18:09 +0300

    added VoteDescription class (stub)

---
+++ model/src/main/java/ru/wiseman/jvote/model/VoteDescription.java
@@ -0,0 +1,11 @@
+package ru.wiseman.jvote.model;
+
+import java.io.Serializable;
+
+/**
+ * User: vaysman
+ * Date: 29.10.2009
+ * Time: 16:15:30
+ */
+public class VoteDescription implements Serializable {
+}
